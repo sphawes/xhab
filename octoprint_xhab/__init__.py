caa20c46e0c467bbf25a4d771d366b6ada1af7e7
sphawes <sphawes@gmail.com>
2017-02-12 15:21:28 -0500

    changed some params in init

--- octoprint_xhab/__init__.py
+++ octoprint_xhab/__init__.py
@@ -7,7 +7,5 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin):
     def on_after_startup(self):
         self._logger.info("Hello World!")
 
-__plugin_name__ = "Hello World"
-__plugin_version__ = "1.0.0"
-__plugin_description__ = "A quick \"Hello World\" example plugin for OctoPrint"
+__plugin_name__ = "UConn XHAB Project"
 __plugin_implementation__ = HelloWorldPlugin()
