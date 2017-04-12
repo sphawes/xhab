43f59e0aa4850dc2494fe5513acb5706175d0016
sphawes <sphawes@gmail.com>
2017-04-12 11:40:30 -0400

    added automation.py, which will handle the request from our new tab and send the appropriate serial string

--- arduino/automationDriver/automationDriver.ino
+++ arduino/automationDriver/automationDriver.ino
@@ -1,7 +1,28 @@
+int scraperStep = 6;
+int scraperDir  = 5;
+
+int filamentStep = 7;
+int filamentDir = 8;
+
+String incomingByte = "";
+
+#include <Servo.h>
+
+Servo myservo;
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  incomingByte = "";
+
+  myservo.attach(11);
+  
+  pinMode(scraperStep, OUTPUT);
+  pinMode(scraperDir, OUTPUT);
+
+  pinMode(filamentStep, OUTPUT);
+  pinMode(filamentDir, OUTPUT);
+  
 
 }
 
@@ -10,12 +31,12 @@ void loop() {
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
-
+    int incomingInt = incomingByte.toInt();
     // say what you got:
     Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
+    Serial.println(incomingInt);
 
-    switch(incomingByte){
+    switch(incomingInt){
       case '1':
         //if we received a 1, then the raspi is trying to get us to engage the linear actuator
         linearActuator(1);
@@ -45,38 +66,62 @@ void loop() {
   }
 }
 
-void linearActuator(action){
+void linearActuator(int action){
   if(action == 0){
     //disengage linear actuator
-
+    myservo.write(150);
   }
   else{
     //engage linear actuator
-
+    myservo.write(82);
   }
-
+  
 }
 
-void scraperCarriage(action){
+void scraperCarriage(int action){
   if(action == 0){
     //raise carriage
-
+    digitalWrite(scraperDir, HIGH);
+    for(int i = 0;i<50;i++){
+      digitalWrite(scraperStep, HIGH);
+      delay(2);
+      digitalWrite(scraperStep, LOW);
+      delay(2);
+    }
   }
   else{
     //lower carriage
-
+    digitalWrite(scraperDir, LOW);
+    for(int i = 0;i<50;i++){
+      digitalWrite(scraperStep, HIGH);
+      delay(2);
+      digitalWrite(scraperStep, LOW);
+      delay(2);
+    }
   }
 
 }
 
-void extrudeFilament(action){
+void extrudeFilament(int action){
   if(action == 0){
-    //stop extruding
-
+    //raise carriage
+    digitalWrite(filamentDir, HIGH);
+    for(int i = 0;i<50;i++){
+      digitalWrite(filamentStep, HIGH);
+      delay(2);
+      digitalWrite(filamentStep, LOW);
+      delay(2);
+    }
   }
   else{
-    //start extruding
-
+    //lower carriage
+    digitalWrite(filamentDir, LOW);
+    for(int i = 0;i<50;i++){
+      digitalWrite(filamentStep, HIGH);
+      delay(2);
+      digitalWrite(filamentStep, LOW);
+      delay(2);
+    }
   }
 
 
